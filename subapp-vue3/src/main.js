32ec77b312c1aa7a491ec66534054dadf6c39dbe
nana <nana.zheng@rccchina.com>
2023-04-12 16:42:43 +0800

    create public-path.js

--- subapp-vue3/src/main.js
+++ subapp-vue3/src/main.js
@@ -1,23 +1,42 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import './public-path' // 引入public-path.js
+// import routes from './router'
+// import store from './store'
+import VueRouter from 'vue-router'
 // createApp(App).mount('#app')
 
+Vue.config.productionTip = false
 let instance = null;
 
-function render() {
-  instance = createApp(App).mount('#app');
+function render(props = {}) {
+  const { container, routerBase } = props
+  const router = new VueRouter({
+    base: window.__POWERED_BY_QIANKUN__ ? routerBase : process.env.BASE_URL,
+    mode: 'history',
+    routes
+  })
+  instance = new Vue({
+    // router,
+    // store,
+    render: (h) => h(App)
+  }).$mount(container ? container.querySelector('#app') : '#app')
 }
-// 开发环境下直接渲染
-if (process.env.NODE_ENV === 'development') {
-  render();
+if (!window.__POWERED_BY_QIANKUN__) {
+  render()
 }
 
 // 导出渲染函数，qiankun 会调用此函数渲染子应用
-export async function bootstrap() {}
-export async function mount() {
-  render();
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped')
+}
+export async function mount(props) {
+  console.log('[vue] props from main framework', props)
+
+  render(props)
 }
 export async function unmount() {
   instance.$destroy();
+  instance.$el.innerHTML = '';
+  instance = null;
 }
--- subapp-vue3/vue.config.js
+++ subapp-vue3/vue.config.js
@@ -1,5 +1,17 @@
 const packageName = require('./package.json').name;
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-})
+
+module.exports = {
+  configureWebpack: {
+    output: {
+      library: `${packageName}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${packageName}`,
+    }
+  },
+  devServer: {
+    port: process.env.VUE_APP_PORT, // 在.env中VUE_APP_PORT=7788，与父应用的配置一致
+    headers: {
+      'Access-Control-Allow-Origin': '*' // 主应用获取子应用时跨域响应头
+    }
+  }
+}
